3de37a392d1ab1333efdc1f1f80b236169b23db2
Lester Emerson <lesphilly@gmail.com>
2018-02-11 00:30:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
-  return `It's ${days}, days until ${holiday}`;
+  return `It\'s ${days} days until ${holiday}!`;
 }
 
